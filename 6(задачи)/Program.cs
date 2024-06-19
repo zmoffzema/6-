543e105c8fceaa7059235556ea187a2eddd1cd07
z_moff <zmoffzema@gmail.com>
2024-06-20 00:12:46 +0300

    Добавьте файлы проекта.

---
+++ 6(задачи)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Запрос числа
+        Console.Write("Введите число: ");
+        string input = Console.ReadLine();
+
+        // Преобразование введенного значения в число
+        if (int.TryParse(input, out int number))
+        {
+            // Проверка, равно ли число 5 или 10
+            if (number == 5 || number == 10)
+            {
+                Console.WriteLine("Число либо равно 5, либо равно 10.");
+            }
+            else
+            {
+                Console.WriteLine("Неизвестное число.");
+            }
+        }
+        else
+        {
+            // Сообщение об ошибке при некорректном вводе
+            Console.WriteLine("Некорректный ввод. Пожалуйста, введите число.");
+        }
+    }
+}
